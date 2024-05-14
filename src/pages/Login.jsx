2af8a20f0dfb8559f81f7ff855767a65ed953780
anhuynh219 <anhuynh200319@gmail.com>
2024-05-14 16:09:31 +0700

    Update logout admin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,17 +2,17 @@ import {Form, Image, Button } from 'react-bootstrap';
 import {Col, Container} from 'react-bootstrap';
 // import logo from 'assets/image/logo2.svg';
 import 'styles/pages/Login.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useLogIn } from 'hooks/useLogin';
 function Login() {
-  const { logIn } = useLogIn();
+  const { logIn, email, passwordLog } = useLogIn();
   const [username, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const handleSubmit = (e) => {
+  const [message, setMessage] = useState('');
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    logIn(username, password);
-    // console.log(username, password);
+    await logIn(username, password);
+    setMessage(localStorage.getItem('error'));
   }
 
   const handleChangeEmail = (e) => {
@@ -32,18 +32,17 @@ function Login() {
           <Form.Group className="mb-3 input" controlId="formBasicEmail">
               <Form.Control type="email" name='email' placeholder="Điền email" onChange={handleChangeEmail} />
               <Form.Text className="text-muted">
+              {message === "Không tìm thấy tài khoản!" ? message : ''}
               </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3 input" controlId="formBasicPassword">
               <Form.Control type="password" name='password' placeholder="Mật khẩu" onChange={handleChangePassword} />
+              <Form.Text className="text-muted">
+              {message === 'Sai mật khẩu!' ? message : ''}
+              </Form.Text>
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <h6 className='forgot_pass'>Bạn quên mật khẩu ?</h6>
-          </Form.Group>
-
-          
           <div className="d-grid gap-2">
             <Button className='login_btn' size='lg' onClick={handleSubmit}>
               Đăng nhập
